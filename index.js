d8ac592916e354a989be11db48432f2a1da075b4
Manju KC <Manju KC>
2019-05-28 19:47:21 -0400

    get the post object with the specified id

--- index.js
+++ index.js
@@ -8,10 +8,27 @@ server.listen(5000, () =>
 );
 
 
-server.get("/", (req, res) => {
+server.get("/posts", (req, res) => {
     db.find()
       .then(post => {
         res.status(200).json(post);
+      })
+      .catch(err => {
+        res.status(500).json({ error: err, message: "Could not secure" });
+      });
+  });
+
+  server.get("/posts/:id", (req, res) => {
+    const postid = req.params.id;
+    db.findById(postid)
+      .then(id => {
+        if (id) {
+          db.findById(postid).then(findId => {
+            res.status(200).json(findId);
+          });
+        } else {
+          res.status(404).json({ message: " No ID found" });
+        }
       })
       .catch(err => {
         res.status(500).json({ error: err, message: "Could not secure" });
