d2518cf702620fa95323c2209d2c7d212275724e
Manju KC <Manju KC>
2019-05-28 20:20:22 -0400

    created a post using the information send inside the request body

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const server = express();
+server.use(express.json());
 
 const db = require('./data/db')
 
 server.listen(5000, () =>
   console.log('Server running on http://localhost:5000')
 );
+
+server.post("/posts", (req, res) => {
+    const { title, contents } = req.body;
+    if (!title || !contents) {
+      res.status(400).json({
+        errorMessage: "Please provide title and contents for the post." 
+      });
+    } else {
+      db.insert({ title, contents })
+        .then(post => {
+          res.status(201).json(post);
+        })
+        .catch(err => {
+          res.status(500).json({
+            error: err,
+            message: "There was an error while saving the post to the database"
+          });
+        });
+    }
+  });
 
 
 server.get("/posts", (req, res) => {
@@ -34,3 +55,5 @@ server.get("/posts", (req, res) => {
         res.status(500).json({ error: err, message: "Could not secure" });
       });
   });
+
+
